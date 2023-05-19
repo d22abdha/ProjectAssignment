d1946310a6e939edd7792eff557c96b9f586c4fb
Abdishakur <Dhagaxg@gmail.com>
2023-05-19 16:09:29 +0200

    Skapat 5 JSON-objekt data som lagrad i webbtjänsten.

---
+++ app/src/main/java/com/example/projectassignment/Country.java
@@ -0,0 +1,77 @@
+package com.example.projectassignment;
+
+import com.google.gson.annotations.SerializedName;
+
+
+/*
+
+ID":"d22abdha_1","name":"Sverige","type":"d22abdha","company":
+"Europa","location":"","category":"","size":0,"cost":10000000,"auxdata":""}
+ */
+public class Country {
+
+    String ID;
+    String name;
+    int Size;
+    String location;
+    @SerializedName("cost")
+    int population;
+    String company;
+    String auxdata;
+
+
+    public Country(String ID, String name,int Size, String location, int population,
+                   String company, String auxdata)
+    {
+        this.ID = ID;
+        this.name = name;
+        this.Size = Size;
+        this.location = location;
+        this.population = population;
+        this.company = company;
+        this.auxdata = auxdata;
+
+    }
+
+    public String ID() {
+
+        return ID;
+    }
+
+
+    public String getName() {
+
+        return name;
+    }
+
+    public int getSize() {
+
+        return Size;
+    }
+
+    public String getLocation() {
+
+        return location;
+    }
+    public int getPopulation() {
+
+        return population;
+    }
+
+
+
+    public String getCompany() {
+
+        return company;
+    }
+
+    public String getAuxdata() {
+
+         return auxdata;
+    }
+
+
+
+
+}
+
