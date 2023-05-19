02ae832eeebf1d4936a7c5be1f3c5e38e712710f
Abdishakur <Dhagaxg@gmail.com>
2023-05-19 15:38:48 +0200

    Skapat andra activity som beskriver appens målgrupp "about"-skärm.

---
+++ app/src/main/java/com/example/projectassignment/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.example.projectassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    //TextView secondTextView
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String name = extras.getString("");
+
+            TextView secondTextView = findViewById(R.id.secondTextView);
+           secondTextView.setText(name);
+
+        }
+
+
+    }
+}
