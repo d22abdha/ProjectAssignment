bcb82bcf6e9ac421fa59c0ce33e6dfaca7b9c97f
Abdishakur <Dhagaxg@gmail.com>
2023-05-19 15:28:00 +0200

    skapat huvudskärm med en lista (RecyclerView) med JSON-data i main activity class

---
+++ app/src/main/java/com/example/projectassignment/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.projectassignment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+@SuppressWarnings("FieldCanBeLocal")
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+    private ArrayList<Country> countries;
+    private RecyclerView recyclerView;
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=d22abdha";
+
+    private RecyclerAdapter adapter;
+    private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        //button test
+        button = findViewById(R.id.sendButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        recyclerView = findViewById(R.id.recyclerView);
+        countries = new ArrayList<>();
+        setAdapter();
+        new JsonTask(this).execute(JSON_URL);
+    }
+
+    private void setAdapter(){
+        adapter = new com.example.projectassignment.RecyclerAdapter(countries);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onPostExecute(String json) {
+        Log.d("MainActivity", json);
+        Gson gson = new Gson();
+        Type type = new TypeToken<ArrayList<Country>>() {}.getType();
+        ArrayList<Country> countries = gson.fromJson(json, type);
+        adapter.addCountries(countries);
+    }
+}
